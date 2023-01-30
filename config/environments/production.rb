3cbc7c7d4ef6d6ca6b3f64fc23e8bca83c87e599
bbasanta238 <bbasanta238@gmail.com>
2023-01-30 22:26:22 +0545

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,17 +91,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  host = "example.com" #replace with your own url
-  config.action_mailer.default_url_options = { host: host }
-
   # SMTP settings for gmail
-  config.action_mailer.smtp_settings = {
-    :address => "bhandaribasanta.com.np",
-    :port => 587,
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = "portfolio-wvw8.onrender.com"
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.sendgrid.net",
+    :port => "587",
+    :authentication => :plain,
     :user_name => ENV["SENDER_MAIL"],
     :password => ENV["SENDER_PASSWORD"],
-    :authentication => "plain",
+    :domain => "render.com",
     :enable_starttls_auto => true,
   }
 end
