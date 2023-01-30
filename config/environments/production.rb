7b75ba44fd321d713c6bd521e0682c8732f31641
bbasanta238 <bbasanta238@gmail.com>
2023-01-30 22:17:20 +0545

    mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => "bhandaribasanta.com.np",
     :port => 587,
     :user_name => ENV["SENDER_MAIL"],
     :password => ENV["SENDER_PASSWORD"],
