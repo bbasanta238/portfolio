183eba0e2319728fb832196d0c24b7a2cf9cdbbb
bbasanta238 <bbasanta238@gmail.com>
2023-01-30 22:12:45 +0545

    mailer config

--- app/mailers/mail_to_admin_mailer.rb
+++ app/mailers/mail_to_admin_mailer.rb
@@ -1,7 +1,7 @@
 class MailToAdminMailer < ApplicationMailer
   def create_message(message:)
     @message = message
-    mail from: ENV["SENDER_MAIL"]
+    mail from: ENV["MAIL_SENDER"]
     mail to: ENV["ADMIN_EMAIL"], subject: @message.subject
   end
 end
