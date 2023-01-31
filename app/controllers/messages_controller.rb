70ca8ed29c710a23d1d35b3392147d0e2428e0c6
bbasanta238 <bbasanta238@gmail.com>
2023-01-31 23:39:49 +0545

    change column phone to phone number

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,7 +13,7 @@ class MessagesController < ApplicationController
     {
       name: params[:name],
       email: params[:email],
-      phone: params[:phone],
+      phone_number: params[:phone],
       subject: params[:subject],
       text: params[:text],
     }
