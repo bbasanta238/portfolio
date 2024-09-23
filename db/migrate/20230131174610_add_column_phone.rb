433d9ef85d739f70d87197c6cdaa5811bf7c2bfc
bbasanta238 <bbasanta238@gmail.com>
2024-09-23 09:41:52 +0545

    update gem file

--- db/migrate/20230131174610_add_column_phone.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnPhone < ActiveRecord::Migration[7.0]
-  def up
-    remove_column :messages, :phone, :integer
-  end
-
-  def down
-    add_column :messages, :phone_number, :string
-  end
-end
